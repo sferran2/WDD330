c4b2837c18054444ca277d45710558b587c84273
eloyou <angelobcaringal@gmail.com>
2025-05-14 00:01:22 +0800

    fix error

--- src/js/product.js
+++ src/js/product.js
@@ -6,7 +6,7 @@ const dataSource = new ProductData("tents");
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product);
-  setLocalStorage("so-cart", product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {

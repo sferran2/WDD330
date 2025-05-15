7dbfabee291d37609bf4865f72eb935b03c7c487
Delpipi <alexandrepaulkouame@gmail.com>
2025-05-15 11:25:19 +0000

    completed activity week02

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,13 +1,41 @@
-class ProductDetails {
+import { setLocalStorage } from "./utils.mjs";
+export default class ProductDetails {
+
     constructor(productId, dataSource) {
         this.productId = productId;
         this.product = {};
         this.dataSource = dataSource;
     }
-    init() {
-        
+
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails(this.product);
+        document.querySelector('#addToCart')
+            .addEventListener('click', this.addProductToCart.bind(this));
     }
-    addProductToCart(product) {
-        setLocalStorage("so-cart", product);
+
+    renderProductDetails(product) {
+        const clone = document.querySelector('#product-cart').content.cloneNode(true);
+        
+        const h3 = clone.querySelector('h3');
+        h3.textContent = product.Brand.Name;
+
+        const h2 = clone.querySelector('h2');
+        h2.textContent = product.NameWithoutBrand;
+
+        const img = clone.querySelector('img');
+        img.setAttribute('src', product.Image);
+        img.setAttribute('alt', product.Name);
+
+        const pList = clone.querySelectorAll('p');
+        pList[0].textContent = `$${product.FinalPrice}`;
+        pList[1].textContent = product.Colors[0].ColorName;
+        pList[2].innerHTML = product.DescriptionHtmlSimple;
+        
+        document.querySelector('#main').appendChild(clone);
+    }
+
+    addProductToCart() {
+        setLocalStorage("so-cart", this.product);
     }
 }
--- src/js/cart.js
+++ src/js/cart.js
@@ -7,6 +7,7 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
+  console.log(item);
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img

1025bd60a5ad7ef96f79fbbfa89be26629b931a1
eloyou <angelobcaringal@gmail.com>
2025-05-15 13:55:37 +0800

    commit changes to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const productList = new ProductList();
+
+const element = document.querySelector(".product-list");
+
+const productList = new ProductList("Tents", dataSource, element);
+
+productList.init();

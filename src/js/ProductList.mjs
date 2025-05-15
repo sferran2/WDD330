27ebae7402416b6ed0f22c4a592a624ff4647ddf
eloyou <angelobcaringal@gmail.com>
2025-05-15 14:31:07 +0800

    fix productList error

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,5 +1,19 @@
 import {renderListWithTemplate} from "./utils.mjs"
-
+function productCardTemplate(product) {
+    return `
+    <li class="product-card">
+        <a href="product_pages/?product=${product.Id}">
+        <img
+         src="${product.Image}"
+         alt="${product.Name}"
+        >
+        <h2 class="card__brand">${product.Brand.Name}</h2>
+        <h3 class="card__name">${product.Name}</h3>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+    
+    </li>
+    `;
+}
 export default class ProductList {
     constructor(category, dataSource, listElement) {
         this.category = category,
@@ -9,6 +23,7 @@ export default class ProductList {
 
     async init() {
         const list = await this.dataSource.getData();
+        this.renderList(list);
     }
 
     renderList(list) {
@@ -21,18 +36,3 @@ export default class ProductList {
 
 }
 
-function productCardTemplate(product) {
-    return `
-    <li class="product-card">
-        <a href="product_pages/?product=${product.id}">
-        <img
-         src="${product.Image}"
-         alt="${product.Name}"
-        >
-        <h2 class="card__brand">${product.Brand.Name}</h2>
-        <h3 class="card__name">${product.Name}</h3>
-        <p class="product-card__price">$${product.FinalPrice}</p>
-    
-    </li>
-    `;
-}

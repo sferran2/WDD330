ce5535fcb61c09d589e0a77031a0535b332db177
jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-05-17 15:34:40 -0500

    albert

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,7 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 
 console.log(dataSource.findProductById(productId));
 

00d98b76334c474eaa0704a8b7afedece910977c
jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-05-23 10:21:33 -0500

    reader

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,23 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
+  const cartItems = getLocalStorage("so-cart") || []; 
   setLocalStorage("so-cart", cartItems);
 }
-// add to cart button event handler
+
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,23 +1,39 @@
-// wrapper for querySelector...returns matching element
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const product = urlParams.get('product')
+
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-// save data to local storage
+
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-// set a listener for both touchend and click
+
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
     callback();
   });
   qs(selector).addEventListener("click", callback);
+}
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product
+}
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+ 
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }

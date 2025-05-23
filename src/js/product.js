cf348257e0ecb46b9363d14063a135277ea94808
eloyou <angelobcaringal@gmail.com>
2025-05-24 00:05:52 +0800

    done stretch activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { getParam} from "./utils.mjs";
+import { getParam, loadHeaderFooter} from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./productDetails.mjs";
 
+loadHeaderFooter();
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
+
